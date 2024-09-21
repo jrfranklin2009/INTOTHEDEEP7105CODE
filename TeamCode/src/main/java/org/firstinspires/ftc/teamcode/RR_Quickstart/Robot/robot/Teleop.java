c942f3ea6e103031a6993d0883c9739de8684ddf
philip <philipdeucher07@gmail.com>
2024-09-21 19:46:48 -0400

    Adding the tele we used for testing the worm gear hang.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR_Quickstart/Robot/robot/Teleop.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.RR_Quickstart.Robot.robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class Teleop extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor hang;
+
+        init();
+
+
+        hang = hardwareMap.get(DcMotor.class,"hang");
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+            if (gamepad1.a) {
+                hang.setPower(1);
+            } else if (gamepad1.b) {
+                hang.setPower(-1);
+            } else {
+                hang.setPower(0);
+            }
+        }
+
+    }
+}
