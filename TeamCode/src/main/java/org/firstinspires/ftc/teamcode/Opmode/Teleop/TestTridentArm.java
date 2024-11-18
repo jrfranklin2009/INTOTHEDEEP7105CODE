a64ff61611f589c8ed9e25d64535df26a9e367af
swift <swiftftc15830>
2024-11-18 18:52:55 -0500

    cleaned up the trident code a little

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/Teleop/TestTridentArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/Teleop/TestTridentArm.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Disabled
 @TeleOp
@@ -23,10 +24,6 @@ public class TestTridentArm extends LinearOpMode {
 
         waitForStart();
         while(opModeIsActive()){
-//            ExtendTridentArm.setPower(0.3*gamepad1.left_trigger);
-//            TurnTridentArm.setPower(0.3*gamepad1.right_trigger);
-//            ExtendTridentArm.setPower(-0.3*gamepad1.left_bumper);
-//            TurnTridentArm.setPower(-0.3*gamepad1.right_bumper);
 
             if (gamepad1.a){
                 isReverse = true;
@@ -36,12 +33,14 @@ public class TestTridentArm extends LinearOpMode {
             }
 
             if (!isReverse){
-                ExtendTridentArm.setPower(0.3*gamepad1.left_trigger);
-                TurnTridentArm.setPower(0.3*gamepad1.right_trigger);
+                ExtendTridentArm.setPower(0.3*gamepad1.left_trigger+0.05);
+                TurnTridentArm.setPower(0.3*gamepad1.right_trigger+0.05);
             }
             if (isReverse){
-                ExtendTridentArm.setPower(-0.3* gamepad1.left_trigger);
-                TurnTridentArm.setPower(-0.3* gamepad1.right_trigger);
+                ExtendTridentArm.setDirection(DcMotorSimple.Direction.REVERSE);
+                TurnTridentArm.setDirection(DcMotorSimple.Direction.REVERSE);
+                ExtendTridentArm.setPower(0.3*gamepad1.left_trigger+0.05);
+                TurnTridentArm.setPower(0.3*gamepad1.right_trigger+0.05);;
             }
         }
     }
