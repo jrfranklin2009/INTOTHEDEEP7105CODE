1918c00789c76ae2ade4f450a6559be4cde30476
swift <swiftftc15830>
2024-12-21 14:34:29 -0500

    Hey

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/Autos/BlueClipSide4.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Opmode.Autos;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.CommandFrameWork.BaseAuto;
+import org.firstinspires.ftc.teamcode.CommandFrameWork.Command;
+import org.firstinspires.ftc.teamcode.CommandFrameWork.CommandScheduler;
+import org.firstinspires.ftc.teamcode.CommandFrameWork.MultipleCommand;
+import org.firstinspires.ftc.teamcode.Robot.robot.Subsystems.ClipMech.ClipMech;
+import org.firstinspires.ftc.teamcode.Robot.robot.Subsystems.DepositingMechanisms.HorizontalSlides;
+import org.firstinspires.ftc.teamcode.Robot.robot.Subsystems.Intake.JohnsIntake;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Config
+@Autonomous
+public class BlueClipSide4 extends BaseAuto {
+    @Override
+    public Command runAuto(CommandScheduler scheduler) {
+        Command runpath;
+
+        robot.driveTrain.mecanumDrive.setPoseEstimate(new Pose2d(-15.8, 59.7, Math.toRadians(90)));
+
+        TrajectorySequence trajectory = robot.driveTrain.mecanumDrive.trajectorySequenceBuilder(new Pose2d(-15.8, 59.7, Math.toRadians(90)))
+                .lineToLinearHeading(new Pose2d(-10, 46, Math.toRadians(90)))
+                .build();
+
+        runpath = new MultipleCommand(RoadRunnerPathSequence(trajectory)).addNext(groups.moveClipMechanismsOut(0, ClipMech.ArmStates.Out_The_Way,
+                HorizontalSlides.HorizontalSlideStates.Half_Out,190, JohnsIntake.ArmStates.preauto_clip));
+        return runpath;
+    }
+}
